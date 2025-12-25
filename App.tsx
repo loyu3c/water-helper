20ca4889f071787c6388c96ae1fd0894bf284c9a
Loyu <loyu3c@gmail.com>
2025-12-26 00:28:16 +0800

    feat: add UI to display error messages with an animated pulse effect

--- App.tsx
+++ App.tsx
@@ -372,6 +372,15 @@ const App: React.FC = () => {
                 </button>
               </div>
             )}
+
+            {error && (
+              <div className="mt-4 p-4 bg-red-50 text-red-600 rounded-xl border border-red-100 flex items-center gap-2 animate-pulse">
+                <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
+                  <path fillRule="evenodd" d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-7 4a1 1 0 11-2 0 1 1 0 012 0zm-1-9a1 1 0 00-1 1v4a1 1 0 102 0V6a1 1 0 00-1-1z" clipRule="evenodd" />
+                </svg>
+                {error}
+              </div>
+            )}
           </div>
         </div>
       </main>
